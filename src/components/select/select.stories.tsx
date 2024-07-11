b5ad29ebf48709daf78d38de0ab6ac21a384619b
web-serge <web_serge@outlook.com>
2024-07-11 12:17:56 +0300

    IN-7 feat: add select

---
+++ src/components/select/select.stories.tsx
@@ -0,0 +1,48 @@
+import type { Meta } from "@storybook/react"
+
+import { Select } from "./select"
+
+const meta = {
+  component: Select,
+  parameters: {
+    layout: "centered"
+  },
+  tags: ["autodocs"],
+  title: "Components/Select"
+} satisfies Meta<typeof Select>
+
+export default meta
+
+export const Default = {
+  args: {
+    options: [
+      { text: "10", value: "10" },
+      { text: "50", value: "50" },
+      { text: "100", value: "100" }
+    ],
+    placeholder: "Select-box"
+  }
+}
+
+export const Open = {
+  args: {
+    open: true,
+    options: [
+      { text: "10", value: "10" },
+      { text: "50", value: "50" },
+      { text: "100", value: "100" }
+    ],
+    placeholder: "Select-box"
+  }
+}
+export const Disabled = {
+  args: {
+    disabled: true,
+    options: [
+      { text: "10", value: "10" },
+      { text: "50", value: "50" },
+      { text: "100", value: "100" }
+    ],
+    placeholder: "Select-box"
+  }
+}
