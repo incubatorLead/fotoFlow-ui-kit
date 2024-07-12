9c1da1e73ececbeca0a4c2ffb124048da089a03e
SergFediaev <sergfediaev@gmail.com>
2024-07-13 02:34:09 +0300

    feat: add input, input styles, input stories. Added storybook-addon-pseudo-states to display pseudo states in stories.

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -21,15 +21,22 @@ type Props = {
 export const Input = forwardRef<HTMLInputElement, Props>((props, ref) => {
   const { className, error, id, labelText, type = "text", ...restProps } = props
 
-  const inputId = id ?? useId()
+  const generatedId = useId()
+  const inputId = id ?? generatedId
   const isInputPassword = type === "password"
   const isInputSearch = type === "search"
 
   const [isPasswordHidden, setPasswordHidden] = useState(isInputPassword)
-  const hidePasswordTitle = isPasswordHidden ? "Показать пароль" : "Скрыть пароль"
-  const inputType =
-    isInputPassword && isPasswordHidden ? "password" : isInputPassword ? "text" : type
   const toggleHidePassword = () => setPasswordHidden(!isPasswordHidden)
+  const hidePasswordTitle = isPasswordHidden ? "Показать пароль" : "Скрыть пароль"
+
+  let inputType
+
+  if (isInputPassword) {
+    inputType = isPasswordHidden ? "password" : "text"
+  } else {
+    inputType = type
+  }
 
   return (
     <div className={clsx(s.inputContainer, className)}>
