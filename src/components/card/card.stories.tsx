877c475953c75d493e3b88c5ea152c7b99f22201
SergFediaev <sergfediaev@gmail.com>
2024-07-12 14:01:07 +0300

    feat: add card, card styles, card stories.

---
+++ src/components/card/card.stories.tsx
@@ -0,0 +1,30 @@
+import type { Meta } from "@storybook/react"
+
+import { Card } from "./card"
+
+const meta = {
+  component: Card,
+  tags: ["autodocs"],
+  title: "Components/Card"
+} satisfies Meta<typeof Card>
+
+export default meta
+
+export const CardWithoutText = {
+  name: "Card without text"
+}
+
+export const CardWithShortText = {
+  args: {
+    children: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+  },
+  name: "Card with short text"
+}
+
+export const CardWithLongText = {
+  args: {
+    children:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In semper lectus risus, vel iaculis arcu tincidunt hendrerit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam tincidunt, magna et congue sodales, nibh erat dapibus augue, quis rutrum diam tortor at metus. Aliquam iaculis malesuada diam nec accumsan. Duis accumsan placerat ligula, sed fringilla tellus sodales rhoncus. Duis pulvinar libero eu porta aliquet. Aliquam erat volutpat. Nam cursus pretium risus a rhoncus. Morbi purus quam, fringilla non ante sed, gravida commodo ligula. Vivamus commodo ex in tortor pharetra varius. Curabitur consequat tellus tortor, at consequat ligula rhoncus a. Mauris convallis pretium dui at interdum.\n\nMaecenas semper tortor at mauris rutrum, et faucibus dui euismod. Nulla dapibus quam nec condimentum scelerisque. Maecenas libero odio, consectetur at metus sit amet, dapibus placerat tellus. Curabitur nec urna sit amet nunc viverra bibendum. Morbi accumsan eros libero, nec fermentum neque posuere in. Suspendisse potenti. Aenean et est orci. Morbi quis ultrices metus, in pulvinar nunc. Nunc vulputate sem lacus, ac accumsan sapien luctus vel. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras nec lobortis odio. Phasellus gravida velit in bibendum auctor. Nulla sit amet lectus ut turpis vestibulum commodo sit amet sit amet sem."
+  },
+  name: "Card with long text"
+}
--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,3 +1,21 @@
-export function Card() {
-  return <div>card</div>
+import type { ComponentPropsWithoutRef, ElementType } from "react"
+
+import clsx from "clsx"
+
+import s from "./card.module.scss"
+
+const DEFAULT_TYPE = "div"
+
+type Props<T extends ElementType = typeof DEFAULT_TYPE> = {
+  as?: T
+} & ComponentPropsWithoutRef<T>
+
+export const Card = <T extends ElementType = typeof DEFAULT_TYPE>({
+  as,
+  className,
+  ...restProps
+}: Props<T>) => {
+  const Component = as ?? DEFAULT_TYPE
+
+  return <Component className={clsx(s.card, className)} {...restProps} />
 }
