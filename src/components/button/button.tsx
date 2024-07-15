eb9bbbb03aca4970be1d9a407b324fd363551f0d
Anton Abrosimov <113538603+Abrosimov96@users.noreply.github.com>
2024-07-15 11:30:08 +0300

    IN-16v2 refactor: use scss variables (#10)
    IN-16v2 refactor: use scss variables, change button props, icon forward in children

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,4 +1,4 @@
-import type { ComponentPropsWithoutRef, ElementType, ReactNode } from "react"
+import type { ComponentPropsWithoutRef, ElementType } from "react"
 
 import clsx from "clsx"
 
@@ -9,21 +9,17 @@ type ButtonVariant = "outlined" | "primary" | "secondary" | "text"
 type ButtonProps<T extends ElementType = "button"> = {
   as?: T
   fullWidth?: boolean
-  icon?: ReactNode
   variant?: ButtonVariant
 } & ComponentPropsWithoutRef<T>
 
 export const Button = <T extends ElementType = "button">(props: ButtonProps<T>) => {
-  const { as, children, className, fullWidth, icon, variant = "primary", ...restProps } = props
+  const { as, className, fullWidth, variant = "primary", ...restProps } = props
   const Component = as ?? "button"
 
   return (
     <Component
       className={clsx(s.button, s[variant], fullWidth && s.fullWidth, className)}
       {...restProps}
-    >
-      {icon}
-      {children && <span>{children}</span>}
-    </Component>
+    />
   )
 }
