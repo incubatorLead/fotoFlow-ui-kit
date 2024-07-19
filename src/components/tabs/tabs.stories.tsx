f63f9d032ec118d5d65fb69e5c6309470d195a4a
Anton Abrosimov <113538603+Abrosimov96@users.noreply.github.com>
2024-07-19 12:02:06 +0300

    IN-27 feat: add tabs component (#19)
    IN-27 feat: add tabs component

---
+++ src/components/tabs/tabs.stories.tsx
@@ -0,0 +1,45 @@
+import type { Meta, StoryObj } from "@storybook/react"
+
+import { Button } from "../button"
+import { TabContent, Tabs } from "./tabs"
+
+const meta = {
+  args: {
+    children: (
+      <>
+        <TabContent value={"uploaded_files"}>Uploaded files</TabContent>
+        <TabContent value={"payments"}>
+          <Button variant={"outlined"}>Payment</Button>
+        </TabContent>
+        <TabContent value={"followers"}>Followers</TabContent>
+      </>
+    ),
+    defaultValue: "payments",
+    tabs: [
+      { title: "Uploaded files", value: "uploaded_files" },
+      { title: "Payments", value: "payments" },
+      { title: "Followers", value: "followers" }
+    ]
+  },
+  component: Tabs,
+  parameters: {
+    layout: "default"
+  },
+  tags: ["autodocs"],
+  title: "Components/Tabs"
+} satisfies Meta<typeof Tabs>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const DefaultTabs: Story = {}
+
+export const PrimaryWithDisabledTab = {
+  args: {
+    tabs: [
+      { title: "Uploaded files", value: "uploaded_files" },
+      { title: "Payments", value: "payments" },
+      { disabled: true, title: "Followers", value: "followers" }
+    ]
+  }
+}
