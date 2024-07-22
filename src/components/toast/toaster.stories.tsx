5d10dfcccb0cbaab2332e00904a87df42bf8de65
Vitalii <vitalii.hryshko.rss@gmail.com>
2024-07-22 18:38:10 +0200

    IN-6 fix: remove jupm effect on story

--- src/components/toast/toaster.stories.tsx
+++ src/components/toast/toaster.stories.tsx
@@ -21,41 +21,43 @@ type Story = StoryObj<typeof Toaster>
 export const ToastsWithControl: Story = {
   render: function Render() {
     return (
-      <div style={{ display: "flex", gap: "10px" }}>
-        <Button
-          onClick={() =>
-            toast.success("Your settings are saved", {
-              cancel: {
-                label: <Icon iconId={"close"} />,
-                // TODO is it possible to remove onClick
-                onClick: () => console.log("Cancel!")
-              },
-              duration: 5000,
-              position: "bottom-left"
-            })
-          }
-          style={{ backgroundColor: "#0a6638" }}
-        >
-          Show Success Toast
-        </Button>
-        <Button
-          onClick={() =>
-            toast.error("Error! Server is not available", {
-              cancel: {
-                label: <Icon iconId={"close"} />,
-                // TODO is it possible to remove onClick
-                onClick: () => console.log("Cancel!")
-              },
-              duration: 5000,
-              position: "bottom-left"
-            })
-          }
-          style={{ backgroundColor: "#660a1d" }}
-        >
-          Show Error Toast
-        </Button>
+      <>
+        <div style={{ display: "flex", gap: "10px" }}>
+          <Button
+            onClick={() =>
+              toast.success("Your settings are saved", {
+                cancel: {
+                  label: <Icon iconId={"close"} />,
+                  // TODO is it possible to remove onClick
+                  onClick: () => console.log("Cancel!")
+                },
+                duration: 5000,
+                position: "bottom-left"
+              })
+            }
+            style={{ backgroundColor: "#0a6638" }}
+          >
+            Show Success Toast
+          </Button>
+          <Button
+            onClick={() =>
+              toast.error("Error! Server is not available", {
+                cancel: {
+                  label: <Icon iconId={"close"} />,
+                  // TODO is it possible to remove onClick
+                  onClick: () => console.log("Cancel!")
+                },
+                duration: 5000,
+                position: "bottom-left"
+              })
+            }
+            style={{ backgroundColor: "#660a1d" }}
+          >
+            Show Error Toast
+          </Button>
+        </div>
         <Toaster />
-      </div>
+      </>
     )
   }
 }
